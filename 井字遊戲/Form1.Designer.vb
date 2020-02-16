704adf986b1f713bf372ac20ced90028f7adbe4c
Leon Chang <skyniceguy@gm.hccvs.hc.edu.tw>
2020-02-16 13:14:28 +0800

    第一版程式上傳

---
+++ 井字遊戲/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.Font = New System.Drawing.Font("新細明體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(34, 116)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 40)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Font = New System.Drawing.Font("新細明體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(95, 116)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 40)
+        Me.Label2.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label3.Font = New System.Drawing.Font("新細明體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label3.Location = New System.Drawing.Point(156, 116)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(31, 40)
+        Me.Label3.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label4.Font = New System.Drawing.Font("新細明體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label4.Location = New System.Drawing.Point(34, 176)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 40)
+        Me.Label4.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label5.Font = New System.Drawing.Font("新細明體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label5.Location = New System.Drawing.Point(95, 176)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 40)
+        Me.Label5.TabIndex = 4
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label6.Font = New System.Drawing.Font("新細明體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label6.Location = New System.Drawing.Point(156, 176)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(31, 40)
+        Me.Label6.TabIndex = 5
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label7.Font = New System.Drawing.Font("新細明體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label7.Location = New System.Drawing.Point(34, 236)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(31, 40)
+        Me.Label7.TabIndex = 6
+        '
+        'Label8
+        '
+        Me.Label8.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label8.Font = New System.Drawing.Font("新細明體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label8.Location = New System.Drawing.Point(95, 236)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(31, 40)
+        Me.Label8.TabIndex = 7
+        '
+        'Label9
+        '
+        Me.Label9.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label9.Font = New System.Drawing.Font("新細明體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label9.Location = New System.Drawing.Point(156, 236)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(31, 40)
+        Me.Label9.TabIndex = 8
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(33, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(72, 60)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "按我開始"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.BackColor = System.Drawing.Color.Black
+        Me.Label10.Font = New System.Drawing.Font("標楷體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label10.ForeColor = System.Drawing.Color.White
+        Me.Label10.Location = New System.Drawing.Point(220, 19)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(193, 29)
+        Me.Label10.TabIndex = 10
+        Me.Label10.Text = "尚未分出勝負"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.BackColor = System.Drawing.Color.LightGreen
+        Me.Label11.Font = New System.Drawing.Font("標楷體", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label11.ForeColor = System.Drawing.Color.White
+        Me.Label11.Location = New System.Drawing.Point(220, 70)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(124, 27)
+        Me.Label11.TabIndex = 11
+        Me.Label11.Text = "經過秒數"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(124, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(63, 60)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "按我結束"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label12.Font = New System.Drawing.Font("標楷體", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label12.ForeColor = System.Drawing.Color.Aqua
+        Me.Label12.Location = New System.Drawing.Point(219, 121)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(159, 35)
+        Me.Label12.TabIndex = 13
+        Me.Label12.Text = "玩家編號"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(526, 304)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "井字遊戲"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label12 As Label
+End Class
---
+++ 井字遊戲/Form1.vb
@@ -0,0 +1,202 @@
+﻿Public Class Form1
+
+    Dim a1, a2, a3, a4, a5, a6, a7, a8, a9 As Short '這個是紀錄X
+    Dim b1, b2, b3, b4, b5, b6, b7, b8, b9 As Short '這個是紀錄O
+    Dim turnwho1 As Short '判定是哪個玩家贏
+    Dim ltime As Integer = 0 '計時器
+
+    '清空所有資料，重新開始
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+        ltime = 0
+        Label11.Text = "經過" & 0 & "秒"
+        Label10.Text = "尚未分出勝負"
+
+        Label1.Text = ""
+        Label2.Text = ""
+        Label3.Text = ""
+
+        Label4.Text = ""
+        Label5.Text = ""
+        Label6.Text = ""
+
+        Label7.Text = ""
+        Label8.Text = ""
+        Label9.Text = ""
+
+        a1 = 0 : a2 = 0 : a3 = 0 : a4 = 0 : a5 = 0 : a6 = 0 : a7 = 0 : a8 = 0 : a9 = 0
+        b1 = 0 : b2 = 0 : b3 = 0 : b4 = 0 : b5 = 0 : b6 = 0 : b7 = 0 : b8 = 0 : b9 = 0
+    End Sub
+
+    '給予判定玩家的初始值
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        turnwho1 = 1
+    End Sub
+
+    '啟動計時器
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+        Timer2.Enabled = True
+    End Sub
+
+    '以秒計時
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        ltime += 1
+        Label11.Text = "經過" & ltime & "秒"
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        '判斷X跟O是否連成一直線
+        If turnwho1 = 1 Then
+            Select Case 3
+                Case a1 + a2 + a3
+                    Label10.Text = "X連成一直線啦！"
+                Case a4 + a5 + a6
+                    Label10.Text = "X連成一直線啦！"
+                Case a7 + a8 + a9
+                    Label10.Text = "X連成一直線啦！"
+                Case a1 + a4 + a7
+                    Label10.Text = "X連成一直線啦！"
+                Case a2 + a5 + a8
+                    Label10.Text = "X連成一直線啦！"
+                Case a3 + a6 + a9
+                    Label10.Text = "X連成一直線啦！"
+                Case a3 + a5 + a7
+                    Label10.Text = "X連成一直線啦！"
+                Case a1 + a5 + a9
+                    Label10.Text = "X連成一直線啦！"
+                Case Else
+                    Label10.Text = "尚未分出勝負"
+            End Select
+        Else
+            Select Case 6
+                Case b1 + b2 + a3
+                    Label10.Text = "Y連成一直線啦！"
+                Case b4 + b5 + b6
+                    Label10.Text = "Y連成一直線啦！"
+                Case b7 + b8 + b9
+                    Label10.Text = "Y連成一直線啦！"
+                Case b1 + b4 + b7
+                    Label10.Text = "Y連成一直線啦！"
+                Case b2 + b5 + b8
+                    Label10.Text = "Y連成一直線啦！"
+                Case b3 + b6 + b9
+                    Label10.Text = "Y連成一直線啦！"
+                Case b3 + b5 + b7
+                    Label10.Text = "Y連成一直線啦！"
+                Case b1 + b5 + b9
+                    Label10.Text = "Y連成一直線啦！"
+                Case Else
+                    Label10.Text = "尚未分出勝負"
+            End Select
+        End If
+
+
+
+
+
+    End Sub
+
+    '點一下的標籤共享事件
+    Private Sub Label_Click(sender As Object, e As EventArgs) Handles Label1.Click, Label2.Click, Label3.Click, Label4.Click, Label5.Click, Label6.Click, Label7.Click, Label8.Click, Label9.Click
+        If ltime = 0 Then
+            MsgBox("開沒開始喔~請點選按我開始")
+            Exit Sub
+        End If
+
+        Select Case sender.tabindex
+            Case 0
+                Label1.Text = "X"
+                a1 = 1
+                b1 = 0
+            Case 1
+                Label2.Text = "X"
+                a2 = 1
+                b2 = 0
+            Case 2
+                Label3.Text = "X"
+                a3 = 1
+                b3 = 0
+            Case 3
+                Label4.Text = "X"
+                a4 = 1
+                b4 = 0
+            Case 4
+                Label5.Text = "X"
+                a5 = 1
+                b5 = 0
+            Case 5
+                Label6.Text = "X"
+                a6 = 1
+                b6 = 0
+            Case 6
+                Label7.Text = "X"
+                a7 = 1
+                b7 = 0
+            Case 7
+                Label8.Text = "X"
+                a8 = 1
+                b8 = 0
+            Case 8
+                Label9.Text = "X"
+                a9 = 1
+                b9 = 0
+            Case Else
+                MsgBox("要點白色區域喔！！")
+        End Select
+        turnwho1 = 1 '換判定X玩家是否連成一直線
+        Label12.Text = "換玩家O"
+    End Sub
+
+    '點二下的標籤共享事件
+    Private Sub Label_DoubleClick(sender As Object, e As EventArgs) Handles Label1.DoubleClick, Label2.DoubleClick, Label3.DoubleClick, Label4.DoubleClick, Label5.DoubleClick, Label6.DoubleClick, Label7.DoubleClick, Label8.DoubleClick, Label9.DoubleClick
+        If ltime = 0 Then
+            MsgBox("開沒開始喔~請點選按我開始")
+            Exit Sub
+        End If
+
+        Select Case sender.tabindex
+            Case 0
+                Label1.Text = "O"
+                b1 = 2
+                a1 = 0
+            Case 1
+                Label2.Text = "O"
+                b2 = 2
+                a2 = 0
+            Case 2
+                Label3.Text = "O"
+                b3 = 2
+                a3 = 0
+            Case 3
+                Label4.Text = "O"
+                b4 = 2
+                a4 = 0
+            Case 4
+                Label5.Text = "O"
+                b5 = 2
+                a5 = 0
+            Case 5
+                Label6.Text = "O"
+                b6 = 2
+                a6 = 0
+            Case 6
+                Label7.Text = "O"
+                b7 = 2
+                a7 = 0
+            Case 7
+                Label8.Text = "O"
+                b8 = 2
+                a8 = 0
+            Case 8
+                Label9.Text = "O"
+                b9 = 2
+                a9 = 0
+            Case Else
+                MsgBox("要點白色區域喔！！")
+        End Select
+        turnwho1 = 2 '換判定O玩家是否連成一直線
+        Label12.Text = "換玩家X"
+    End Sub
+End Class
